ab4dd03665bbd154cbc1d572a244ecf76ecc7345
ragingpenguin <joshuahuntereflin@gmail.com>
2026-04-12 08:43:08 -0400

    instructions  are corrected

--- app/components/terminal/instructions.tsx
+++ app/components/terminal/instructions.tsx
@@ -2,10 +2,10 @@ export const Instructions = () => {
 
     return (
         <article className="instructions">
-            <div> Welcome to the Command line!</div>
-            <div> You can Navigate to different pages using standard unix commands</div>
+            <div> Welcome to the command line!</div>
+            <div> You can navigate to different pages using standard unix commands</div>
             <div> if you don&apos;t know what that means, type &quot;help&quot;</div>
-            <div> Type Resume to Open My resume in a new Tab</div>
+            <div> Type resume to open my resume in a new tab</div>
         </article>
     )
 }
